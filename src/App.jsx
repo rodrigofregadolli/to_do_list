2899054c45b50d02940877b6b674f3913beb7e0a
Rodrigo Fregadolli <rodrigofregadolli@hotmail.com>
2024-01-30 09:41:00 -0300

    criação da funcionalidade de busca

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 import Todo from "./components/Todo";
-
 import "./App.css";
 import TodoForm from './components/TodoForm';
+import Search from './components/search';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -26,6 +26,8 @@ function App() {
   isCompleted: false,
  },
 ]);
+
+const [search, setSearch] = useState("");
 
 const addTodo = (text, category) => {
   const newTodos = [
@@ -59,9 +61,19 @@ const completeTodo = (id) => {
  return (
 <div className="app">
  <h1>Lista de Tarefas</h1>
+ <Search search={search} setSearch={setSearch} />
  <div className="todo-list"> 
-   {todos.map((todo) => (
-      <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
+   {todos
+    .filter((todo) => 
+    todo.text.toLowerCase().includes(search.toLowerCase())
+   )
+    .map((todo) => (
+      <Todo 
+      key={todo.id} 
+      todo={todo} 
+      removeTodo={removeTodo} 
+      completeTodo={completeTodo}
+    />
    ))}
   </div>
   <TodoForm addTodo={addTodo} />
